041cacecb28a7fbf451fd92263e820eb5db456a8
Kidong Lee <mykidong@gmail.com>
2015-05-29 22:13:49 +0900

    .

--- src/main/java/jcascalog/example/JsonToParquetWrite.java
+++ src/main/java/jcascalog/example/JsonToParquetWrite.java
@@ -1,8 +1,6 @@
 package jcascalog.example;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
